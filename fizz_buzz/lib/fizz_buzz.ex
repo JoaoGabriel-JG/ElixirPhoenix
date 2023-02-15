de08fb14b8ec7dc7d2671eac388b92c715b3a14d
João Gabriel <joao.nitsus@gmail.com>
2023-02-15 10:42:30 -0300

    att

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -8,8 +8,20 @@ defmodule FizzBuzz do
   def handle_file_read({ :ok, result }) do
     result
     |> String.split(",")   # "1,2,3,4" -> ["1", "2", "3", "4"]
-    |> Enum.map(&String.to_integer/1)    # ["1", "2", "3", "4"] -> [1, 2, 3, 4]
+    |> Enum.map(&convert_and_evaluate_numbers/1)
   end
 
   def handle_file_read({ :error, reason }), do: "Error reading the file: #{reason}"
+
+  def convert_and_evaluate_numbers(elem) do
+    number = String.to_integer(elem)
+    evaluate_numbers(number)
+  end
+
+  def evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  def evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  def evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  def evaluate_numbers(number), do: number
 end
+
+# O Patern Matching é Sequencial
