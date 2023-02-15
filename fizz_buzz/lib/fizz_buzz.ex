b40a5645f0d76e9b9e0b7fd1a163adbe2604fa5a
João Gabriel <joao.nitsus@gmail.com>
2023-02-15 09:37:12 -0300

    att

---
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -0,0 +1,10 @@
+defmodule FizzBuzz do
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read()
+  end
+
+  def handle_file_read({ :ok, result }), do: result
+  def handle_file_read({ :error, reason }), do: reason
+end
