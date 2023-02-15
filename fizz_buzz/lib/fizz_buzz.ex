d28077ea235a333cb25d0a5307f11f6a512d0c6a
João Gabriel <joao.nitsus@gmail.com>
2023-02-15 10:13:36 -0300

    att

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -5,6 +5,11 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({ :ok, result }), do: result
-  def handle_file_read({ :error, reason }), do: reason
+  def handle_file_read({ :ok, result }) do
+    result
+    |> String.split(",")   # "1,2,3,4" -> ["1", "2", "3", "4"]
+    |> Enum.map(&String.to_integer/1)    # ["1", "2", "3", "4"] -> [1, 2, 3, 4]
+  end
+
+  def handle_file_read({ :error, reason }), do: "Error reading the file: #{reason}"
 end
