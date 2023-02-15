e6cf244c33843678d7d92f463e79740524b89045
João Gabriel <joao.nitsus@gmail.com>
2023-02-15 10:56:45 -0300

    att

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -5,23 +5,28 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({ :ok, result }) do
-    result
+
+  defp handle_file_read({ :ok, result }) do
+    result =
+      result
     |> String.split(",")   # "1,2,3,4" -> ["1", "2", "3", "4"]
     |> Enum.map(&convert_and_evaluate_numbers/1)
+
+    { :ok, result }
   end
 
-  def handle_file_read({ :error, reason }), do: "Error reading the file: #{reason}"
+  defp handle_file_read({ :error, reason }), do: { :error, "Error reading the file: #{reason}" }
 
-  def convert_and_evaluate_numbers(elem) do
-    number = String.to_integer(elem)
-    evaluate_numbers(number)
+  defp convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_numbers()
   end
 
-  def evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
-  def evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
-  def evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
-  def evaluate_numbers(number), do: number
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_numbers(number), do: number
 end
 
 # O Patern Matching é Sequencial
