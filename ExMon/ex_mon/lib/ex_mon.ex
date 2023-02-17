3981fe59ee6755e55681d44e5238034bdd5effa3
João Gabriel <joao.nitsus@gmail.com>
2023-02-17 11:45:16 -0300

    att

--- ExMon/ex_mon/lib/ex_mon.ex
+++ ExMon/ex_mon/lib/ex_mon.ex
@@ -1,7 +1,15 @@
 defmodule ExMon do
-  alias ExMon.Player # , as: Banana
+  alias ExMon.{Game, Player}
+
+  @computer_name "Joadson"
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player(:mouseGamerPunch, :notebookBackflipKick, :heal)
+    |>Game.start(player)
   end
 end
