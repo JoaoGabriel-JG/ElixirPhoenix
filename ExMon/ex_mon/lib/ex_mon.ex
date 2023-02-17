0c04118a4958eccde782ebabf3df9430c490fa8a
João Gabriel <joao.nitsus@gmail.com>
2023-02-17 09:22:13 -0300

    att

--- ExMon/ex_mon/lib/ex_mon.ex
+++ ExMon/ex_mon/lib/ex_mon.ex
@@ -1,7 +1,7 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.Player # , as: Banana
 
-  def create_player layer(name, move_avg, move_rnd, move_heal) do
+  def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
   end
 end
