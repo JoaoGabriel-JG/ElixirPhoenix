eb1704e2a4f2e85eb28cac1008a6e2b42b82896c
JoaoGabriel-JG <joao.nitsus@gmail.com>
2023-02-27 10:31:15 -0300

    att

--- ExMon/ex_mon/lib/game/status.ex
+++ ExMon/ex_mon/lib/game/status.ex
@@ -1,5 +1,8 @@
-def module ExMon.Game.Status do
+defmodule ExMon.Game.Status do
+  alias ExMon.Game
   def print_round_message() do
-    IO.puts("The game is started")
+    IO.puts("\n======== The game is started! ========\n")
+    IO.inspect(Game.info())
+    IO.puts("-----------------------------------")
   end
 end
